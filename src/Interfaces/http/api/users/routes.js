fab0acd1e04db10cf52f38ea721726cb190bb71c
XrerXrerX <dikihidayat/dh@gmail.com>
2024-03-09 12:35:40 +0700

    push register player only handler not yet handler testing 11

--- src/Interfaces/http/api/users/routes.js
+++ src/Interfaces/http/api/users/routes.js
@@ -3,6 +3,11 @@ const routes = (handler) => ([
     method: 'POST',
     path: '/users',
     handler: handler.postUserHandler,
+    options: {
+      cors: {
+        origin: ['*'],
+      },
+    },
   },
 ]);
 
