2a12298199613d5b56d7b2de9abfa137ab0946a6
XrerXrerX <dikihidayat/dh@gmail.com>
2024-05-12 19:39:03 +0700

    add auth and cors for any path in routes 6

--- src/Infrastructures/http/createServer.js
+++ src/Infrastructures/http/createServer.js
@@ -16,14 +16,9 @@ const createServer = async (container) => {
   const server = Hapi.server({
     host: process.env.HOST,
     port: process.env.PORT,
-    routes: {
-      cors: {
-        additionalHeaders: ['Origin', 'Access-Control-Allow-Origin'],
-        additionalExposedHeaders: ['Origin', 'Access-Control-Allow-Origin'],
-        credentials: true,
-
-      },
-    },
+    "routes": {
+      "cors": true
+    }
   });
 
 
@@ -79,6 +74,7 @@ const createServer = async (container) => {
     // mendapatkan konteks response dari request
     const { response } = request;
 
+
     if (response instanceof Error) {
       // bila response tersebut error, tangani sesuai kebutuhan
       const translatedError = DomainErrorTranslator.translate(response);
@@ -109,34 +105,6 @@ const createServer = async (container) => {
       newResponse.code(500);
       return newResponse;
     }
-    // Handle CORS
-    const corsHeaders = {
-      'Access-Control-Allow-Methods': 'OPTIONS, GET, POST, PUT, DELETE',
-      'Access-Control-Allow-Headers': 'Content-Type, Authorization',
-    };
-
-    // Check if CORS header is present in the response
-    const responseHeaders = response.headers || {};
-    const hasCorsHeader = Object.keys(responseHeaders).some(header =>
-      header.toLowerCase() === 'access-control-allow-origin'
-    );
-
-    if (!hasCorsHeader) {
-      // If CORS header is not present, allow access from all origins
-      corsHeaders['Access-Control-Allow-Origin'] = '*';
-    }
-
-    if (request.method === 'options') {
-      // Handling preflight request
-      return h.response().code(200).headers(corsHeaders);
-    }
-
-    // Apply CORS headers to response
-    const responseWithCors = response.isBoom ? response.output : response;
-    responseWithCors.headers = {
-      ...responseWithCors.headers,
-      ...corsHeaders,
-    };
 
     // jika bukan error, lanjutkan dengan response sebelumnya (tanpa terintervensi)
     return h.continue;
