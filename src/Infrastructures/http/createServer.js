6b332aab711ddd3006a988050b86e3e1991fc7c5
XrerXrerX <dikihidayat/dh@gmail.com>
2024-05-28 00:21:14 +0700

    minor bug cors

--- src/Infrastructures/http/createServer.js
+++ src/Infrastructures/http/createServer.js
@@ -32,6 +32,10 @@ const createServer = async (container) => {
         noSniff: true, // Enable protection against clickjacking attacks
         xframe: true, // Enable protection against cross-site request forgery (CSRF)
       },
+      cors: {
+        origin: ['*'], // Default value, bisa Anda sesuaikan dengan nilainya.
+        additionalHeaders: ['x-customblhdrs', 'Origin'], // Tambahkan 'Origin' ke header tambahan untuk CORS.
+      },
     },
   });
   // Create a logger
